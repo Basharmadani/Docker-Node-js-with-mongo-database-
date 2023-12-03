599ccef0b84d94e6b9c59855638c8d3ec1931482
unknown <Basharmadani7@gmail.com>
2023-12-03 20:05:01 +0300

    Node

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-
 FROM  node:alpine 
-
-
-
 WORKDIR  /app
 COPY package.json .
-RUN npm install 
+RUN npm install
+ 
 COPY . .
+
+EXPOSE 4000 
 CMD [ "npm", "start" ]
--- home.js
+++ home.js
@@ -1,28 +1,23 @@
 const express = require('express');
-const redis = require('redis');
+const mongoose = require('mongoose');
+
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 27017;
+
+// ip for container mongo ex
+const DB_HOST = 'mongodb';
+
+
+const DATABASE_NAME = 'mydatabase';
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DATABASE_NAME}`;
+
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to db .....'))
+  .catch((err) => console.log('OPS failed', err));
+
+const port = process.env.PORT || 4000;
 const app = express();
-const client =redis.createClient(
-    {
-
-        host:'redis',
-        port:6379,
-        
-    }
-
-);
-
-client.set('visitCounter',0);
-app.get('/',(req, res) => {client.get('visitCounter' ,(err,visitCounter)=>{
-
-res.send('visitCounter:' + visitCounter);
-client.set('visitCounter',parseInt(visitCounter)+1);
-
-
-})
-})
-app.listen(8080,( )=>{
-    console.log('listening on port 8080'); 
-
-})
-
-
+app.get('/', (req, res) => res.send('<h1> hello</h1>'));
+app.listen(port, () => console.log(`app is up on port :${port} `));
